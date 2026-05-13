507e9d0b596d81865a18e4692b1a1e22b985067e
ResonanceEnergy <gripandripphdd@outlook.com>
2026-05-13 07:40:47 -0600

    wave 2 finish: URP AetherFog shader + RenderGraph feature auto-install

--- Assets/_Project/Scripts/Core/BuildInfo.cs
+++ Assets/_Project/Scripts/Core/BuildInfo.cs
@@ -3,8 +3,8 @@ namespace Tartaria.Core
 {
     public static class BuildInfo
     {
-        public const string Version = "v1.0.0-122-g3002085-dirty";
-        public const string Hash = "3002085";
-        public const string BuildTime = "2026-05-13 05:21:17 UTC";
+        public const string Version = "v1.0.0-123-gf95caec-dirty";
+        public const string Hash = "f95caec";
+        public const string BuildTime = "2026-05-13 13:38:36 UTC";
     }
 }
